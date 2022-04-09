d17b26a83b314de0042f610c806ace80cf625a66
qqq <aswd25@yandex.ru>
2022-04-09 16:25:05 +0200

    html templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path, re_path
+
+from catalog import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    re_path(r'^fighters/$', views.FighterViewList.as_view(), name='fighters'),
+    re_path(r'^fighter/(?P<pk>\d+)$', views.FighterViewDetail.as_view(), name='fighter-detail')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+
+from catalog.models import Fighter
+
+
+def index(request):
+    num_fighters = Fighter.objects.all().count()
+    print(num_fighters)
+    return render(request, 'index.html', context={'num_fighters': num_fighters}, )
+
+class FighterViewList(generic.ListView):
+    model = Fighter
+
+
+class FighterViewDetail(generic.DetailView):
+    model = Fighter
